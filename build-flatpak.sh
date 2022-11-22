2faf4bf96e51c52ceb70f2c158f2113bab67e70c
Tim <tymmesyde@gmail.com>
2022-11-22 16:02:53 +0100

    chore: move flatpak related files to a folder

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock
-flatpak-builder flatpak-build com.stremio.Service.json --force-clean
-flatpak-builder --repo=stremio-flatpak-repo --force-clean flatpak-build com.stremio.Service.json
-flatpak build-bundle stremio-flatpak-repo Service.flatpak com.stremio.Service master
+python flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock
+flatpak-builder flatpak/flatpak-build com.stremio.Service.json --force-clean
+flatpak-builder --repo=flatpak/stremio-flatpak-repo --force-clean flatpak/flatpak-build com.stremio.Service.json
+flatpak build-bundle flatpak/stremio-flatpak-repo flatpak/com.stremio.Service.flatpak com.stremio.Service master

49060c2cada5158ae47d8f2688d2536dd7abc74a
Tim <tymmesyde@gmail.com>
2022-11-23 09:48:15 +0100

    build: remove unecessary flatpak-builder cmd

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock
-flatpak-builder flatpak/flatpak-build com.stremio.Service.json --force-clean
 flatpak-builder --repo=flatpak/stremio-flatpak-repo --force-clean flatpak/flatpak-build com.stremio.Service.json
 flatpak build-bundle flatpak/stremio-flatpak-repo flatpak/com.stremio.Service.flatpak com.stremio.Service master

6cf453bef87de7d9d16067406bb3faa3d94a3c90
Tim <tymmesyde@gmail.com>
2022-11-29 19:49:00 +0100

    build: fix macos archive extraction

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, fs, path::PathBuf, io::Cursor};
 
+use bytes::Bytes;
+use flate2::bufread::GzDecoder;
 use tar::Archive;
 use xz::bufread::XzDecoder;
 
@@ -10,6 +12,22 @@ const NODE_WINDOWS_ARCHIVE: &str = "https://nodejs.org/dist/v18.12.1/node-v18.12
 const NODE_LINUX_ARCHIVE: &str = "https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz";
 #[cfg(target_os = "macos")]
 const NODE_MACOS_ARCHIVE: &str = "https://nodejs.org/dist/v18.12.1/node-v18.12.1-darwin-x64.tar.gz";
+
+trait Decoder {
+    fn new(r: Cursor<Bytes>) -> Self;
+}
+
+impl Decoder for XzDecoder<Cursor<Bytes>> {
+    fn new(r: Cursor<Bytes>) -> Self {
+        XzDecoder::new(r)
+    }
+}
+
+impl Decoder for GzDecoder<Cursor<Bytes>> {
+    fn new(r: Cursor<Bytes>) -> Self {
+        GzDecoder::new(r)
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=src/");
@@ -34,11 +52,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     #[cfg(target_os = "linux")] {
-        extract_tar_xz(NODE_LINUX_ARCHIVE, "node", target_bin_path.clone())?;
+        extract_tar::<XzDecoder<Cursor<Bytes>>>(NODE_LINUX_ARCHIVE, "bin/node", "node", &target_bin_path)?;
     }
 
     #[cfg(target_os = "macos")] {
-        extract_tar_xz(NODE_MACOS_ARCHIVE, "node", target_bin_path.clone())?;
+        extract_tar::<GzDecoder<Cursor<Bytes>>>(NODE_MACOS_ARCHIVE, "bin/node", "node", &target_bin_path)?;
     }
 
     let binaries_dir = current_dir.join("binaries");
@@ -68,17 +86,17 @@ fn extract_zip(url: &str, file_name: &str, out: PathBuf) -> Result<(), Box<dyn E
     Ok(())
 }
 
-fn extract_tar_xz(url: &str, file_name: &str, out: PathBuf) -> Result<(), Box<dyn Error>> {
-    let target = out.join(file_name);
+fn extract_tar<D: Decoder + std::io::Read>(url: &str, file_path: &str, out_name: &str, out: &PathBuf) -> Result<(), Box<dyn Error>> {
+    let target = out.join(out_name);
     if !target.exists() {
         let archive_file = reqwest::blocking::get(url)?.bytes()?;
-        let decoded_stream = XzDecoder::new(Cursor::new(archive_file));
+        let decoded_stream = D::new(Cursor::new(archive_file));
         let mut archive = Archive::new(decoded_stream);
         for entry in archive.entries()? {
             let mut file = entry?;
-            let file_path = file.path()?;
-            if file_path.is_file() && file_path.ends_with(file_name) {
-                file.unpack_in(out.clone())?;
+            let path = file.path()?;
+            if path.ends_with(file_path) {
+                file.unpack(target.clone())?;
             }
         }            
     }

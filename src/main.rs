0516cb37c3718fe4f770f22744032fbd2be671ed
Vladimir Borisov <borisov1024@gmail.com>
2023-04-24 16:00:09 +0300

    Show terminal if debug assertions are enabled on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(
+    all(target_os = "windows", not(debug_assertions),),
+    windows_subsystem = "windows"
+)]
 use anyhow::Context;
 use clap::Parser;
 use std::error::Error;

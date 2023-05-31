7169c4dc3a91206f406afc47cae6b7b801a531c8
Tim <tymmesyde@gmail.com>
2023-05-31 16:18:40 +0200

    build: create single file bundle for flatpak

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock
-flatpak-builder --repo=flatpak/stremio-flatpak-repo --force-clean flatpak/flatpak-build com.stremio.Service.json
-flatpak build-bundle flatpak/stremio-flatpak-repo flatpak/com.stremio.Service.flatpak com.stremio.Service master
+flatpak-builder --force-clean flatpak/build com.stremio.Service.json
+flatpak build-export flatpak/repo flatpak/build
+flatpak build-bundle flatpak/repo flatpak/com.stremio.Service.flatpak com.stremio.Service

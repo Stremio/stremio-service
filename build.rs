76a41e28359b3a9f476580ab4e2e78715fffccd8
Tim <tymmesyde@gmail.com>
2023-06-01 13:18:53 +0200

    build: use server source file for flatpak

--- build.rs
+++ build.rs
@@ -64,53 +64,56 @@ fn main() -> Result<(), Box<dyn Error>> {
     let current_dir = std::env::current_dir()?;
     let platform_bins = current_dir.join("resources").join("bin").join(OS);
 
-    let server_js_target = platform_bins.join("server.js");
-    // keeps track of the server.js version in order to update it if versions mismatch
-    let server_js_version_file = platform_bins.join("server_version.txt");
+    #[cfg(not(feature = "flatpak"))]
+    {
+        let server_js_target = platform_bins.join("server.js");
+        // keeps track of the server.js version in order to update it if versions mismatch
+        let server_js_version_file = platform_bins.join("server_version.txt");
 
-    let manifest_version = {
-        let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
-        let manifest = cargo_toml::Manifest::<Metadata>::from_path_with_metadata(manifest_path)
-            .expect("Cannot read the manifest metadata");
+        let manifest_version = {
+            let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
+            let manifest = cargo_toml::Manifest::<Metadata>::from_path_with_metadata(manifest_path)
+                .expect("Cannot read the manifest metadata");
 
-        let server_metadata = manifest
-            .package
-            .expect("Failed to parse package")
-            .metadata
-            .expect("Failed to parse manifest.package.metadata")
-            .server;
+            let server_metadata = manifest
+                .package
+                .expect("Failed to parse package")
+                .metadata
+                .expect("Failed to parse manifest.package.metadata")
+                .server;
 
-        server_metadata.version
-    };
+            server_metadata.version
+        };
 
-    let download_server_js = || -> Result<(), Box<dyn Error>> {
-        let version_url = STREMIO_SERVER_URL
-            .clone()
-            .join(&format!("{manifest_version}/desktop/server.js"))
-            .expect("Should never fail");
+        let download_server_js = || -> Result<(), Box<dyn Error>> {
+            let version_url = STREMIO_SERVER_URL
+                .clone()
+                .join(&format!("{manifest_version}/desktop/server.js"))
+                .expect("Should never fail");
 
-        let server_js_file = reqwest::blocking::get(version_url)?
-            .error_for_status()?
-            .bytes()?;
+            let server_js_file = reqwest::blocking::get(version_url)?
+                .error_for_status()?
+                .bytes()?;
 
-        fs::write(&server_js_target, server_js_file)?;
-        // replace content in the version file
-        fs::write(&server_js_version_file, &manifest_version)?;
-        Ok(())
-    };
+            fs::write(&server_js_target, server_js_file)?;
+            // replace content in the version file
+            fs::write(&server_js_version_file, &manifest_version)?;
+            Ok(())
+        };
 
-    match (
-        server_js_target.exists(),
-        fs::read_to_string(&server_js_version_file).ok(),
-    ) {
-        // if server.js does not exist (no matter if the version file exist)
-        // or if the server.js file exist but we don't have a version file.
-        (false, _) | (true, None) => download_server_js()?,
-        (true, Some(version)) => {
-            if manifest_version != version {
-                download_server_js()?
+        match (
+            server_js_target.exists(),
+            fs::read_to_string(&server_js_version_file).ok(),
+        ) {
+            // if server.js does not exist (no matter if the version file exist)
+            // or if the server.js file exist but we don't have a version file.
+            (false, _) | (true, None) => download_server_js()?,
+            (true, Some(version)) => {
+                if manifest_version != version {
+                    download_server_js()?
+                }
+                // else do nothing, we have the same version
             }
-            // else do nothing, we have the same version
         }
     }
 

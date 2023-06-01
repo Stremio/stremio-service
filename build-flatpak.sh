8de85732c683decd7478706bfd0ec32b510c42b8
Tim <tymmesyde@gmail.com>
2023-06-01 13:33:09 +0200

    chore: improve project structure related to flatpak

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock
+python flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock -o flatpak/cargo-sources.json
 
 server_version=$(sed -n '/package.metadata.server/,$p' Cargo.toml | grep -m 1 'version =' | awk -F'"' '{print $2}')
 server_file_url=https://dl.strem.io/server/$server_version/desktop/server.js
@@ -9,7 +9,7 @@ server_source_template='{
   "url": "{server_file_url}",
   "sha256": "{sha256_checksum}"
 }'
-echo "$server_source_template" | sed "s|{server_file_url}|$server_file_url|" | sed "s|{sha256_checksum}|$sha256_checksum|" > server-source.json
+echo "$server_source_template" | sed "s|{server_file_url}|$server_file_url|" | sed "s|{sha256_checksum}|$sha256_checksum|" > flatpak/server-source.json
 
 flatpak-builder --force-clean flatpak/build com.stremio.Service.json
 flatpak build-export flatpak/repo flatpak/build

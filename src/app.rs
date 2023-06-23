cfbbf07952765afc7a66469750bf02ba6dbfd549
Lachezar Lechev <lachezar@ambire.com>
2023-06-23 16:07:35 +0300

    fix: macos parse for AddonUrl
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- src/app.rs
+++ src/app.rs
@@ -424,7 +424,7 @@ fn register_apple_event_callbacks() -> fruitbasket::FruitApp<'static> {
         Box::new(move |event| {
             let open_url: String = fruitbasket::parse_url_event(event);
 
-            let open_url = match open_url.parse() {
+            let open_url = match open_url.parse::<AddonUrl>() {
                 Ok(addon_url) => StremioWeb::Addon(open_url).open(),
                 Err(err) => {
                     error!("{err}");

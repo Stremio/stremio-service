42d889191aad57c9ba5464ca3b2da4acc4c6f1e9
Tim <tymmesyde@gmail.com>
2025-09-02 10:40:38 +0200

    chore: cleanup flatpak build file

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-python3 flatpak/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock -o flatpak/cargo-sources.json
+
+cwd="flatpak"
+pkg_id="com.stremio.Service"
+
+python3 $cwd/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock -o $cwd/cargo-sources.json
 
 server_version=$(sed -n '/package.metadata.server/,$p' Cargo.toml | grep -m 1 'version =' | awk -F'"' '{print $2}')
 server_file_url=https://dl.strem.io/server/$server_version/desktop/server.js
@@ -9,10 +13,9 @@ server_source_template='{
   "url": "{server_file_url}",
   "sha256": "{sha256_checksum}"
 }'
-echo "$server_source_template" | sed "s|{server_file_url}|$server_file_url|" | sed "s|{sha256_checksum}|$sha256_checksum|" > flatpak/server-source.json
+echo "$server_source_template" | sed "s|{server_file_url}|$server_file_url|" | sed "s|{sha256_checksum}|$sha256_checksum|" > $cwd/server-source.json
 
-sed -e 's/usr/app/g' -e 's/com.stremio.service/com.stremio.Service/g' resources/com.stremio.service.desktop > flatpak/com.stremio.Service.desktop
+sed -e 's/usr/app/g' -e 's/com.stremio.service/com.stremio.Service/g' resources/com.stremio.service.desktop > $cwd/$pkg_id.desktop
 
-flatpak-builder --force-clean flatpak/build flatpak/com.stremio.Service.json
-flatpak build-export flatpak/repo flatpak/build
-flatpak build-bundle flatpak/repo flatpak/com.stremio.Service.flatpak com.stremio.Service
+flatpak-builder --repo=$cwd/repo --force-clean $cwd/build $cwd/$pkg_id.json
+flatpak build-bundle $cwd/repo $cwd/$pkg_id.flatpak $pkg_id
